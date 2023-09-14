bdda76051bc74836846ff567c666ae8d2d486c1f
rainy <you@example.com>
2023-09-14 23:03:11 +0900

    reduced csv file size

--- mysite/radar/static/radar/script.js
+++ mysite/radar/static/radar/script.js
@@ -1,12 +1,1 @@
 'use strict'
-
-// const btn = document.getElementById('btn');
-// const frame = document.getElementById('frame');
-// const error = document.getElementsByClassName('errorlist')
-// const reset = document.getElementById('reset')
-
-// btn.addEventListener('click', function() {
-    // frame.style.display = 'block';
-    // };
-
-// }, false);
