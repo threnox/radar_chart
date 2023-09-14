4a9fb05232eaa9e396bc0c930ac9e24c04f035de
threnox <50074249+threnox@users.noreply.github.com>
2023-09-15 02:03:33 +0900

    Add files via upload

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'radar'
+    'radar.apps.RadarConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static' # 追加
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
