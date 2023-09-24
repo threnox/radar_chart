b4fa320f46ab29f05b1682b60c90fa3462aa1240
threnox <threnox@gmail.com>
2023-09-24 16:52:58 +0900

    for render

--- .history/mysite/mysite/settings_20230924165201.py
+++ .history/mysite/mysite/settings_20230924165201.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = str(BASE_DIR / 'media')
+MEDIA_ROOT = str(BASE_DIR / "media")
 
 STATIC_URL = 'static/'
 # 追加
