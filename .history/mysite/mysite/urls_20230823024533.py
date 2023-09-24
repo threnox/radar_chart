3b9f3b048e1fb38d3cd67507f3ecdcb15afeb751
threnox <threnox@gmail.com>
2023-09-24 22:18:19 +0900

    for render

--- .history/mysite/mysite/urls_20230823024533.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("radar/", include("radar.urls.test")),
-    path("admin/", admin.site.urls),
-]
--- .history/mysite/mysite/urls_20230823024603.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("radar/", include("radar.urls")),
-    path("admin/", admin.site.urls),
-]
